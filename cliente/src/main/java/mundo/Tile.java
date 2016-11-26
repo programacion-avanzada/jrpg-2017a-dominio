c4bcda57b571fd5eef759807b085ba6e69519c81
Claudio Saccella <cdsaccella@gmail.com>
2016-11-26 00:58:47 -0300

    Aubenor actualizado

--- cliente/src/main/java/mundo/Tile.java
+++ cliente/src/main/java/mundo/Tile.java
@@ -8,8 +8,11 @@ import recursos.Recursos;
 public class Tile {
 	
 	public static Tile[] tiles = new Tile[256];
-	public static Tile cesped = new Tile(Recursos.cesped, 0, false);
-	public static Tile greenTree = new Tile(Recursos.greenTree, 1, true, 64, 70);
+	//public static Tile cesped = new Tile(Recursos.cesped, 0, false);
+	//public static Tile greenTree = new Tile(Recursos.iceBlock, 1, true, 64, 64);
+	public static Tile [] aubenor;
+	
+	public static int aubenorBase = 3;
 	
 	public static final int ANCHO = 64;
 	public static final int ALTO = 32;
@@ -48,10 +51,14 @@ public class Tile {
 		g.drawImage(textura, x, y, ANCHO, ALTO, null);
 	}
 	
-	 public void graficar(Graphics g, int x, int y , int width, int height){
+	public void graficar(Graphics g, int x, int y , int width, int height){
 	    g.drawImage(textura, x, y, width, height, null);
-	 }
+	}
 	 
+	public void setSolido(boolean solidez){
+		esSolido = solidez;
+	}
+	
 	
 	public boolean esSolido() {
 		return esSolido;
@@ -68,5 +75,6 @@ public class Tile {
 	public int getAlto() {
 		return alto;	 
 	}
+
 		 
 }
--- cliente/src/main/resources/recursos/Recursos.java
+++ cliente/src/main/resources/recursos/Recursos.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.Map;
 
 import frames.MenuCarga;
+import mundo.Tile;
 
 public class Recursos {
 
@@ -328,6 +329,32 @@ public class Recursos {
 		greenTree = trees.getTile(0, 0, 42, 50);
 		nievePiso1 = CargadorImagen.cargarImagen("/nieve piso.png");
 		iceBlock = CargadorImagen.cargarImagen("/nieve cubo.png");
+		
+		
+		// Mapa
+		SpriteSheet mapaAubenor = new SpriteSheet(CargadorImagen.cargarImagen("/Aubenor.png"));
+		
+		Tile.aubenor = new Tile[81];
+		
+		boolean [][] solidezAubenor = {{true, true, false, true, false, true, true, true, true, true},
+				{true, true, true, true, true, true, true, true, true, true},
+				{true, true, true, true, true, true, true, true, true, true},
+				{true, false, false, false, false , false, false ,false, true, true},
+				{false, false, false, false, false , false, false ,false, true, true},
+				{false, true, true, true, true, true, true, true, true, true},
+				{true, true, true, true, true, true, true, true, true, true},
+				{true, true, true, true, true, true, true, true, true, true}};	
+		
+		for(int y = 0 ; y < 8 ; y++){
+			
+			for(int x = 0; x < 10 ; x++){
+				
+				Tile.aubenor[y * 10 + x + 1] = new Tile(mapaAubenor.getTile(x * 64, y * 64, 64, 64), y * 10 + x + 1, solidezAubenor[y][x], 64, 64);
+				
+			}
+			
+		}
+		
 		// Fin Entorno
 
 		// Inicio Batalla
