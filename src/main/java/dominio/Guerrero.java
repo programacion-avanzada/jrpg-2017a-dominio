2fdbef8196dd3be88def25c45971914ad1c8159a
Ramiro Laurenzzo <rama.eze.12@gmail.com>
2017-04-21 21:27:55 -0300

    Update Guerrero.java
    Agrego una descripción sobre la clase y sobre 2 metodos ("habilidad1" y "habilidad3")

--- src/main/java/dominio/Guerrero.java
+++ src/main/java/dominio/Guerrero.java
@@ -1,5 +1,10 @@
 package dominio;
 
+/**
+	La clase "Guerrero" es un tipo de casta (por ende, hereda
+	de esta misma). Aqui se describen los metodos propios de
+	esta clase.
+*/
 public class Guerrero extends Casta {
 
 	public Guerrero(double prob_crit, double evasion, double daño_crit) {
@@ -17,6 +22,12 @@ public class Guerrero extends Casta {
 		habilidadesCasta[2] = "Ignorar Defensa";
 	}
 
+/**
+	El metodo "habilidad1" representa la habilidad "Ataque
+	Doble" que equivale a duplicar el ataque causado por el
+	atacante, siempre y cuando el atacante disponga de la
+	suficiente energia. 
+*/
 	// Ataque Doble
 	public boolean habilidad1(Personaje caster, Peleable atacado) { 
 		if (caster.getEnergia() > 10) {
@@ -37,6 +48,12 @@ public class Guerrero extends Casta {
 		return false;
 	}
 
+/**
+	El metodo "habilidad3" representa la habilidad "Ignorar
+	Defensa" que equivale a que la defensa de la victima 
+	se reduzca a 0 para ser atacado. Luego de recibir el
+	ataque, se le devuelve la defensa original. 
+*/
 	// Ignorar Defensa
 	public boolean habilidad3(Personaje caster, Peleable atacado) { 
 		if (caster.getEnergia() > 10) {
