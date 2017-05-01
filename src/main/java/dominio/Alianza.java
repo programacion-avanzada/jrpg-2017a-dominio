976332997a170f3177ab60925afd1cba84dadf4c
Pablo Raimondo <pabloaraimondo@gmail.com>
2017-05-01 13:54:36 -0300

    Se corrige la clase Alianza según gradle y pequeñeses en la clase MadreDeTodo

--- src/main/java/dominio/Alianza.java
+++ src/main/java/dominio/Alianza.java
@@ -1,46 +1,44 @@
+
 package dominio;
 
 import java.util.LinkedList;
 
 public class Alianza {
 
-	String nombre;
-	LinkedList<Personaje> aliados;
-	/**
-	 * La clase Alianza tiene como función agrupar a los jugadores
+	private final String nombre;
+	private LinkedList<Personaje> aliados;
+	/**La clase Alianza tiene como función agrupar a los jugadores.
 	 * mediante una LinkedList de Personajes
-	 * 
 	 * @param nombre Nombre que se le dará a la alianza
 	 */
-	
-	public Alianza(String nombre) {
+
+	public Alianza(final String nombre){
 		this.nombre = nombre;
 		this.aliados = new LinkedList <Personaje>();
 	}
 
-	public LinkedList<Personaje> getAliados() {
+	public final LinkedList<Personaje> getAliados(){
 		return aliados;
 	}
-	/**
-	 * Método void que referencia al atributo aliados con el argumento del método.
+
+	/**Método void que referencia al atributo aliados con el argumento del método.
 	 * De haber una lista aliados preexistente, está será sobreescrita por la nueva.
 	 * @param aliados LinkedList que sobreescribirá a la actual
 	 */
-	
-	public void setAliados(LinkedList<Personaje> aliados) {
+
+	public final void setAliados(final LinkedList<Personaje> aliados){
 		this.aliados = aliados;
 	}
 
-	public String obtenerNombre(){
+	public final String obtenerNombre(){
 		return nombre;
 	}
-	
-	public void eliminarPersonaje(Personaje pj){
+
+	public final void eliminarPersonaje(final Personaje pj){
 		aliados.remove(pj);
 	}
-	
-	/**
-	 * Método void que agrega un nuevo Personaje a la LinkedList aliados utilizando el método
+
+	/**Método void que agrega un nuevo Personaje a la LinkedList aliados utilizando el método.
 	 * add() de la LinkedList
 	 * @param pj Personaje a agregarse a la lista de aliados
 	 */
