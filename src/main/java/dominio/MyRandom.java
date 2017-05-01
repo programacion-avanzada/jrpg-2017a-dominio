a95b2de4ddbe2318b01a8678d6e4e176e541b30e
Pablo Raimondo <pabloaraimondo@gmail.com>
2017-05-01 13:58:29 -0300

    Se corrige la clase MyRandom según gradle

--- src/main/java/dominio/MyRandom.java
+++ src/main/java/dominio/MyRandom.java
@@ -1,26 +1,25 @@
 package dominio;
 
 public class MyRandom {
-	/**
-	 * Clase con dos métodos estaticos, utilizados para la obtención de números de tipo int y double
-	 * 
+	private static final double NUMERORARO = 0.49;
+	private static final int OTRONUMERORARO =1;
+	/**Clase con dos métodos estaticos.
+	 * Utilizados para la obtención de números de tipo int y double
 	 */
-	
-	
-	/**
-	 * El método nextDobule() retorna siempre el número 0.49
+
+
+	/**El método nextDobule() retorna siempre el número 0.49.
 	 * @return retorna 0.49
 	 */
 	public static double nextDouble() {
-		return 0.49;
+		return NUMERORARO;
 	}
-	/**
-	 * El método nextInt() decrementa en 1 el argumento que se le pasó
+	/** El método nextInt() decrementa en 1 el argumento que se le pasó.
 	 * @param val número entero a decrementar
-	 * @return número decrementado 
+	 * @return número decrementado
 	 */
-	public static int nextInt(int val) {
-		return val - 1;
+	public static int nextInt(final int val) {
+		return val - OTRONUMERORARO;
 	}
-	
+
 }
