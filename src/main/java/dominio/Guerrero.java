7e082a6dbf686233ad111926d3a5ee11b878265b
MaxiARG <maxiramos2017@gmail.com>
2017-05-04 14:46:15 -0300

    Check Guerrero

--- src/main/java/dominio/Guerrero.java
+++ src/main/java/dominio/Guerrero.java
@@ -1,64 +1,70 @@
 package dominio;
 
 /**
- * Un tipo de raza que hereda las caracteristicas de Casta
+ * Un tipo de raza que hereda las caracteristicas de Casta.
  *
  */
 public class Guerrero extends Casta {
 
-	public Guerrero(double prob_crit, double evasion, double daño_crit) {
-		super(prob_crit, evasion, daño_crit);
-		this.nombreCasta = "Guerrero";
-	}
+  public Guerrero(double prob_crit, double evasion, double daño_crit) {
+    super(prob_crit, evasion, daño_crit);
+    this.nombreCasta = "Guerrero";
+  }
 
-	public Guerrero() {
-		super();
-		this.nombreCasta = "Guerrero";
+  public Guerrero() {
+    super();
+    this.nombreCasta = "Guerrero";
 
-		habilidadesCasta = new String[3];
-		habilidadesCasta[0] = "Ataque Doble";
-		habilidadesCasta[1] = "Aumentar Defensa";
-		habilidadesCasta[2] = "Ignorar Defensa";
-	}
+    habilidadesCasta = new String[3];
+    habilidadesCasta[0] = "Ataque Doble";
+    habilidadesCasta[1] = "Aumentar Defensa";
+    habilidadesCasta[2] = "Ignorar Defensa";
+  }
 
-	// Ataque Doble
-	/* (non-Javadoc)
-	 * @see dominio.Casta#habilidad1(dominio.Personaje, dominio.Peleable)
-	 * Devuelve si el personaje que le pasamos tiene la habilidad pedida
-	 */
-	public boolean habilidad1(Personaje caster, Peleable atacado) { 
-		if (caster.getEnergia() > 10) {
-			caster.setEnergia(caster.getEnergia() - 10);
-			if (atacado.serAtacado(caster.ataque * 2) > 0)
-				return true;
-		}
-		return false;
-	}
-	
-	// Aumentar Defensa
-	public boolean habilidad2(Personaje caster, Peleable atacado) { 
-		if (caster.getEnergia() > 10) {
-			caster.setEnergia(caster.getEnergia() - 10);
-			caster.setDefensa(caster.getDefensa() + caster.magia);
-			return true;
-		}
-		return false;
-	}
+  // Ataque Doble
+  /*
+   * (non-Javadoc)
+   *
+   * @see dominio.Casta#habilidad1(dominio.Personaje, dominio.Peleable) Devuelve
+   * si el personaje que le pasamos tiene la habilidad pedida
+   */
+  @Override
+  public boolean habilidad1(Personaje caster, Peleable atacado) {
+    if (caster.getEnergia() > 10) {
+      caster.setEnergia(caster.getEnergia() - 10);
+      if (atacado.serAtacado(caster.ataque * 2) > 0) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	// Ignorar Defensa
-	public boolean habilidad3(Personaje caster, Peleable atacado) { 
-		if (caster.getEnergia() > 10) {
-			caster.setEnergia(caster.getEnergia() - 10);
-			if (atacado instanceof Personaje) {
-				int defensa_original = ((Personaje) atacado).getDefensa();
-				((Personaje) atacado).setDefensa(0);
-				if (atacado.serAtacado(caster.ataque) > 0) {
-					((Personaje) atacado).setDefensa(defensa_original);
-					return true;
-				}
-			}
+  // Aumentar Defensa
+  @Override
+  public boolean habilidad2(Personaje caster, Peleable atacado) {
+    if (caster.getEnergia() > 10) {
+      caster.setEnergia(caster.getEnergia() - 10);
+      caster.setDefensa(caster.getDefensa() + caster.magia);
+      return true;
+    }
+    return false;
+  }
 
-		}
-		return false;
-	}
+  // Ignorar Defensa
+  @Override
+  public boolean habilidad3(Personaje caster, Peleable atacado) {
+    if (caster.getEnergia() > 10) {
+      caster.setEnergia(caster.getEnergia() - 10);
+      if (atacado instanceof Personaje) {
+        int defensa_original = ((Personaje) atacado).getDefensa();
+        ((Personaje) atacado).setDefensa(0);
+        if (atacado.serAtacado(caster.ataque) > 0) {
+          ((Personaje) atacado).setDefensa(defensa_original);
+          return true;
+        }
+      }
+
+    }
+    return false;
+  }
 }
