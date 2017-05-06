75d5f5a308a23238eb8979b0df3c7297a730484b
juliancarroccio <juliancarroccio@gmail.com>
2017-05-05 21:30:09 -0300

    se levantan incidencias

--- src/main/java/dominio/MyRandom.java
+++ src/main/java/dominio/MyRandom.java
@@ -1,12 +1,23 @@
 package dominio;
 
+/**
+ *  La clase MyRandom sirve para generar, con nuestras propias reglas, numeros aleatorios
+*/
 public class MyRandom {
+  
+  private static final double DOUBLE_ALEATORIO = 0.49;
 
+  /**
+   *  nextDouble genera un Double aleatorio
+  */
 	public static double nextDouble() {
-		return 0.49;
-	}
-	public static int nextInt(int val) {
-		return val - 1;
+		return DOUBLE_ALEATORIO;
 	}
 	
+	/**
+   *  nextInt genera un Entero aleatorio
+  */
+	public static int nextInt(final int val) {
+		return val - 1;
+	}
 }
