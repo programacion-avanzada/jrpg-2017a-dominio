e104be885e8e68b51005dbaed4e98725567256df
juliancarroccio <juliancarroccio@gmail.com>
2017-05-05 21:28:35 -0300

    se levantan incidencias

--- src/main/java/dominio/Alianza.java
+++ src/main/java/dominio/Alianza.java
@@ -1,46 +1,44 @@
 package dominio;
 
 import java.util.LinkedList;
-/**  Esta clase es desarrollada para saber saber y establecer las alianzas entre los personajes */
 
+/**  Esta clase es desarrollada para saber y establecer las alianzas entre los personajes */
 public class Alianza {
 
-	String nombre;
-	LinkedList<Personaje> aliados;
+  private final String nombre;
+  private final LinkedList<Personaje> aliados;
 
-	public Alianza(String nombre) {
-		this.nombre = nombre;
-		this.aliados = new LinkedList <Personaje>();
-	}
-     /** Este medoto devuelve el listado de los personajes aliados en la alianza actual --*/
-	
-	public LinkedList<Personaje> getAliados() {
-		return aliados;
-	}
-    /**Este medoto une un nuevo personaje a la alianza actual*/
-	
-	public void setAliados(LinkedList<Personaje> aliados) {
-		this.aliados = aliados;
-	}
-    /** Este medoto devuelve el nombre de la alianza actual @David*/ 
-	
-	public String obtenerNombre(){
-		return nombre;
-	}
-	
-	/**
+  public Alianza(String nombre) {
+    this.nombre = nombre;
+    this.aliados = new LinkedList<Personaje>();
+  }
+  
+  /** Este medoto devuelve el nombre de nuestra alianza actual*/
+  public String getNombre() {
+    return nombre;
+  }
+  
+  /** Este medoto devuelve el listado de los personajes aliados en la alianza actual*/
+  public LinkedList<Personaje> getAliados() {
+    return aliados;
+  }
+
+  /** Este medoto devuelve el nombre de la alianza actual @David*/ 
+  public String obtenerNombre() {
+    return nombre;
+  }
+
+  /**
      * Método "eliminarPersonaje", elimina a un personaje que ya no es aliado
-     */	
-	
-	public void eliminarPersonaje(Personaje pj){
-		aliados.remove(pj);
-	}
-	
-	/**
-     * Método "añadirPersonaje", añade a un personaje aliado.
-     */	
-	
-	public void añadirPersonaje(Personaje pj){
-		aliados.add(pj);
-	}
+     */ 
+  public void eliminarPersonaje(final Personaje pj) {
+    aliados.remove(pj);
+  }
+
+  /**
+     * Método "anadirPersonaje", anade a un personaje aliado.
+     */ 
+  public void anadirPersonaje(final Personaje pj) {
+    aliados.add(pj);
+  }
 }
