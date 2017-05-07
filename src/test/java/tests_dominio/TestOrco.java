1db155c713e8f5f5c477aa2f38ae1b978600cf9f
SilvaDiegoUnlam <spookycynicism@gmail.com>
2017-05-07 16:44:18 -0300

    Separacion de tests en orco

--- src/test/java/tests_dominio/TestOrco.java
+++ src/test/java/tests_dominio/TestOrco.java
@@ -15,12 +15,15 @@ public class TestOrco {
 	public void testGolpeDefensivo() {
 		Humano h = new Humano("Nicolas", new Guerrero(), 1);
 		Orco o = new Orco("Hernan", new Guerrero(), 1);
-
 		Assert.assertTrue(h.getSalud() == 105);
-		if (o.habilidadRaza1(h))
-			Assert.assertTrue(h.getSalud() == 95);
-		else
-			Assert.assertTrue(o.getSalud() == 105);
+		o.habilidadRaza1(h);
+		Assert.assertTrue(h.getSalud() == 95);
+	}
+	
+	@Test
+	public void testGolpeDefensivoFallido() {
+		Humano h = new Humano("Nicolas", new Guerrero(), 1);
+		Orco o = new Orco("Hernan", new Guerrero(), 1);
 		o.setEnergia(0);
 		Assert.assertFalse(o.habilidadRaza1(h));
 	}
@@ -29,16 +32,17 @@ public class TestOrco {
 	public void testMordiscoDeVida() {
 		Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
 		Orco o = new Orco("Nico", 100, 100, 80, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
-
 		Assert.assertTrue(h.getSalud() == 100);
 		o.setSalud(100);
-		if (o.habilidadRaza2(h)) {
+		o.habilidadRaza2(h);
 			Assert.assertEquals(40, h.getSalud());
 			Assert.assertTrue(o.getSalud() == 100);
-		} else {
-			Assert.assertTrue(o.getSalud() == 100);
-			Assert.assertTrue(h.getSalud() == 100);
-		}
+	}
+	
+	@Test
+	public void testMordiscoDeVidaFallido() {
+		Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
+		Orco o = new Orco("Nico", 100, 100, 80, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
 		o.setEnergia(0);
 		Assert.assertFalse(o.habilidadRaza2(h));
 	}
