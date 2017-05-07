5b8ee7e3c325c1b6ff58c0dc9c132a9dae76a21f
SilvaDiegoUnlam <spookycynicism@gmail.com>
2017-05-07 18:01:11 -0300

    Separacion de tests en clase asesino

--- src/test/java/tests_dominio/TestAsesino.java
+++ src/test/java/tests_dominio/TestAsesino.java
@@ -16,7 +16,6 @@ public class TestAsesino {
 	public void testCritico(){
 		Humano h = new Humano("Nicolas", new Asesino(), 1);
 		Humano h2 = new Humano("Lautaro", new Hechicero(), 2);
-		
 		Assert.assertEquals(105, h2.getSalud());
 		if	(h.habilidadCasta1(h2))
 			Assert.assertTrue(93==h2.getSalud());
@@ -35,18 +34,31 @@ public class TestAsesino {
 		Assert.assertTrue(0.5==h.getCasta().getProbabilidadEvitarDano());
 	}
 	@Test
-	public void testHabilidades(){
+	public void testHabilidad1Fallida(){
 		Humano h = new Humano("Nicolas", new Asesino(), 1);
 		Humano h2 = new Humano("Lautaro", new Hechicero(), 2);
-		
-		Assert.assertEquals(105, h2.getSalud());
-		if	(h.habilidadCasta1(h2))
-			Assert.assertTrue(93==h2.getSalud());
-		else
-			Assert.assertEquals(105, h2.getSalud());
 		h.setEnergia(0);
-		Assert.assertFalse(h.habilidadCasta1(h2));
-		Assert.assertFalse(h.habilidadCasta2(h2));
+		Assert.assertEquals(105, h2.getSalud());
+		h.habilidadCasta1(h2);
+		Assert.assertEquals(105, h2.getSalud());
+	}
+	
+	@Test
+	public void testhabilidad2Fallida(){
+		Humano h = new Humano("Nico",100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
+		Assert.assertTrue(0.3==h.getCasta().getProbabilidadEvitarDano());
+		h.setEnergia(0);
+		h.habilidadCasta2(null);
+		Assert.assertEquals(0.3, h.getCasta().getProbabilidadEvitarDano(), 0.01);
+		h.habilidadCasta2(null);
+		Assert.assertTrue(0.3==h.getCasta().getProbabilidadEvitarDano());
+	}
+	
+	@Test
+	public void testhabilidad3Fallida(){
+		Humano h = new Humano("Nico",100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
+		Humano h2 = new Humano("Lautaro", new Hechicero(), 2);
 		Assert.assertFalse(h.habilidadCasta3(h2));
 	}
+	
 }
